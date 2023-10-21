cab62009cb5f700795b6a175b7a09b0497b923cf
CrackThaDom <85846263+CracktheDom@users.noreply.github.com>
2023-10-21 13:49:33 -0500

    Add output for CloudFront distribution domain name

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "bucket_name" {
-  description = "Name of S3 bucket"
-  value       = aws_s3_bucket.website.id
+# output "bucket_name" {
+#   description = "Name of S3 bucket"
+#   value       = aws_s3_bucket.website.id
+# }
+
+output "cloudfront_domain_name" {
+  description = "URL of CloudFront distribution"
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+# output s3_bucket_endpoint {
+#   description = "The website endpoint"
+#   value = aws_s3_bucket_website_configuration.website.website_endpoint
+# }
